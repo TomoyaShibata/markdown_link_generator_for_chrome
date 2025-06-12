8fd0c361cfcbd91d3f07d25128040aaf13154b7e
TomoyaShibata <wind.of.hometown@gmail.com>
2025-06-12 15:18:32 +0900

    :+1: Add support for Scrapbox

--- popup.js
+++ popup.js
@@ -1,22 +1,41 @@
-const markdownLinkText = document.getElementById('js-markdown-link-text')
+const markdownText = document.getElementById('js-markdown-text')
+const scrapboxText = document.getElementById('js-scrapbox-text')
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   const tab = tabs[0]
   if (tab) {
-    markdownLinkText.value = `[${tab.title}](${tab.url})`
-    markdownLinkText.select()
+    markdownText.value = `[${tab.title}](${tab.url})`
+    scrapboxText.value = `[${tab.title} ${tab.url}]`
   }
 })
 
-const copyButton = document.getElementById('js-copy-button')
-copyButton.addEventListener('click', () => {
-  navigator.clipboard.writeText(markdownLinkText.value)
+const copyMarkdownButton = document.getElementById('js-copy-markdown-button')
+copyMarkdownButton.addEventListener('click', () => {
+  navigator.clipboard.writeText(markdownText.value)
     .then(() => {
-      const prevTextContent = copyButton.textContent
-      copyButton.textContent = 'COPY DONE!!'
-      copyButton.disabled = true
+      const prevTextContent = copyMarkdownButton.textContent
+      copyMarkdownButton.textContent = 'DONE!'
+      copyMarkdownButton.disabled = true
       setTimeout(() => {
-        copyButton.textContent = prevTextContent
-        copyButton.disabled = false
+        copyMarkdownButton.textContent = prevTextContent
+        copyMarkdownButton.disabled = false
+        window.close()
+      }, 1200)
+    })
+    .catch((err) => {
+      console.error('Failed to copy text: ', err)
+    })
+})
+
+const copyScrapboxButton = document.getElementById('js-copy-scrapbox-button')
+copyScrapboxButton.addEventListener('click', () => {
+  navigator.clipboard.writeText(scrapboxText.value)
+    .then(() => {
+      const prevTextContent = copyScrapboxButton.textContent
+      copyScrapboxButton.textContent = 'DONE!'
+      copyScrapboxButton.disabled = true
+      setTimeout(() => {
+        copyScrapboxButton.textContent = prevTextContent
+        copyScrapboxButton.disabled = false
         window.close()
       }, 1200)
     })
